54d3b2983ba8b59b0abacf56d3adcbb3520d02ae
Rohit-pawar52 <rkps2580@gmail.com>
2026-04-20 11:29:45 +0530

    route brush up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,15 +1,14 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Models\User;
 
-Class UserController extends Controller{
+Class HomeController extends Controller{
     public function index(){
-        $users = User::all();
-        // dd($users);
         return view('home');
     }
 }
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function(){
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('user/list', [UserController::class, 'index'])->name('Userlist');
